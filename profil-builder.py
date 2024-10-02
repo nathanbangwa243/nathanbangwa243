a6d120bd83ee249ebf1ff5f42c20997d5a9288b7
nathanbangwa243 <nathanbangwa7@gmail.com>
2024-10-02 19:15:53 +0200

    [ADD] docs: add certif to linkedin profile pdf

---
+++ profil-builder.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from PyPDF2 import PdfMerger
+
+OUT_PUT_PROFILE_FILE = 'docs/Nathan-Bangwa-Profile-CV'
+
+PROFIL_CERTIF_FILES = [
+    'docs/profile.pdf',
+    'docs/ai-programming-with-python.pdf',
+    'docs/aws-machine-learning-fundamentals.pdf',
+]
+
+# Get today's date and format it
+today_str = datetime.today().strftime('%d%m%y')
+
+# open pdf merger
+merger = PdfMerger()
+
+for certif in PROFIL_CERTIF_FILES:
+    merger.append(certif)
+
+# write pdf merger content
+merger.write(f"{OUT_PUT_PROFILE_FILE}.pdf")
+merger.write(f"{OUT_PUT_PROFILE_FILE}-{today_str}.pdf")
+
+# close pdf merger
+merger.close()
